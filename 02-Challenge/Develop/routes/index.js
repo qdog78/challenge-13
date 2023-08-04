d4730a5b07423040c6ff91cdd130cf31e9dc5d72
qdog78 <qdog78@yahoo.com>
2023-08-03 22:28:37 -0500

    edited a route

--- 02-Challenge/Develop/routes/index.js
+++ 02-Challenge/Develop/routes/index.js
@@ -2,10 +2,12 @@ const router = require('express').Router();
 const apiRoutes = require('./api');
 
 // localhost:3001/api
-router.use('/api', apiRoutes);
 
-router.use((req, res) => {
+router.get('/', (req, res) => {
+  console.log (req.path)
   res.send("<h1>Wrong Route!</h1>")
 });
+
+router.use('/api', apiRoutes);
 
 module.exports = router;
